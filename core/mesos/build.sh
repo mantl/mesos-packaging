8bf29d58ebacf3503f98b8c33731fbfaafe116d3
Zogg <zoggified@gmail.com>
2016-01-15 17:42:58 +0200

    cleanup

--- core/mesos/build.sh
+++ core/mesos/build.sh
@@ -1,4 +1,4 @@
-set -x
+set -ex
 tar -xzf mesos-{{.Version}}.tar.gz
 
 echo `pwd`
@@ -9,43 +9,36 @@ sudo yum install -y protobuf-devel protobuf-java protobuf-python boost-devel
 
 ## create an installation
 INSTALL={{.BuildRoot}}/out
-mkdir $INSTALL || echo "Install dir created"
+mkdir $INSTALL 
 
 # glog
 wget https://github.com/google/glog/archive/v0.3.4.tar.gz
 tar -xzvf v0.3.4.tar.gz
 cd glog-0.3.4
 ./configure
-make &>/dev/null
-sudo make install &> /dev/null
-make install DESTDIR="$INSTALL" &>/dev/null
-#make install DESTDIR=/home/vagrant/test &>> /dev/null
+make 
+sudo make install 
+make install DESTDIR="$INSTALL" 
 cd ..
-
-echo "BUILDING MESOS"
 
 ## build mesos
 cd mesos-{{.Version}}
 ./bootstrap
 mkdir build
 pushd build
-../configure --prefix=/usr --with-protobuf=/usr --with-boost=/usr --with-glog=${INSTALL}/usr/local/ --enable-optimize &> /dev/null
-make -j {{.CPUs}} &>>/dev/null
-make install DESTDIR="$INSTALL" &> /dev/null
-#make install DESTDIR=/home/vagrant/test &> /dev/null
+../configure --prefix=/usr --with-protobuf=/usr --with-boost=/usr --with-glog=${INSTALL}/usr/local/ --enable-optimize 
+make -j {{.CPUs}} 
+make install DESTDIR="$INSTALL" 
 popd
 
-echo "FINISHED BUILDING MESOS"
-
 pushd $INSTALL
-mkdir -p var/log/mesos var/lib/mesos || echo "Dirs created"
+mkdir -p var/log/mesos var/lib/mesos 
 
 # jars
 mkdir -p usr/share/java || echo "dirs for jars"
 cp {{.BuildRoot}}/mesos-{{.Version}}/build/src/java/target/mesos-*.jar usr/share/java
 popd
 
-echo "BUILDING NET MODULES"
 
 ## Net-modules
 git clone https://github.com/mesosphere/net-modules.git -b integration/0.26
@@ -57,14 +50,11 @@ ln -s lib lib64
 popd
 
 ./bootstrap
-mkdir build || echo "build dir for netmodules"
+mkdir build 
 cd build
-../configure --with-mesos=${INSTALL}/usr --with-protobuf=/usr &>> /dev/null
-make >> /dev/null
-echo "INSTALLING NETMODULES" &>> /dev/null
-echo "DESTDIR IS ${INSTALL}" &>> /dev/null
-make install DESTDIR="$INSTALL" &>> /dev/null
-echo "SYMLINKING"
+../configure --with-mesos=${INSTALL}/usr --with-protobuf=/usr 
+make 
+make install DESTDIR="$INSTALL" 
 
 pushd ${INSTALL}/usr/
 rm -f lib64
@@ -72,8 +62,7 @@ popd
 
 pushd $INSTALL
 # symlinks
-mkdir -p usr/local/lib || echo "dir for symlinks"
+mkdir -p usr/local/lib 
 # ensure symlinks are relative so they work as expected in the final env
 ( cd usr/local/lib && cp -s ../../lib/lib*so . )
 
-echo "END"
